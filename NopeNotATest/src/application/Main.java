8a5d9f5292d716e418ebe43f8b730fcd89be2633
barce <barce@BarMobile.attlocal.net>
2023-03-06 20:11:28 -0600

    added button 2

--- NopeNotATest/src/application/Main.java
+++ NopeNotATest/src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
+import java.util.ArrayList;
 import java.util.Random;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,18 +17,35 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		
+		ArrayList<Label> labelList = new ArrayList<>();
+		
 		Button b = new Button("Button");
 		b.setLayoutX(50);
 		b.setLayoutY(50);
 		
+		Button c = new Button("Button2");
+		c.setLayoutX(150);
+		c.setLayoutY(50);
+		
 		Pane p = new Pane();
 		p.setPrefSize(400, 400);
-		p.getChildren().add(b);
+		p.getChildren().addAll(b,c);
 		b.setOnAction(e->{
 			Label l = new Label("You pressed the button, why?");
 			l.setLayoutX(new Random().nextDouble(300));
 			l.setLayoutY(new Random().nextDouble(400));
+			labelList.add(l);
 			p.getChildren().add(l);
+		});
+		
+		c.setOnAction(e->{
+			
+				for(Node l : p.getChildren()) {
+						p.getChildren().remove(l);
+						
+					}
+				
+			
 		});
 		
 		try {
